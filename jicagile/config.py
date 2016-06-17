3ac2611788703c5d2be1b9f1a73f7be4d81242ea
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2016-06-17 20:21:16 +0100

    Refactored Team class to make it easier to add Themes

--- jicagile/config.py
+++ jicagile/config.py
@@ -2,20 +2,12 @@
 
 import yaml
 
-
-class Team(dict):
-    """Class representing an agile team."""
-
-    class Member(object):
-        """Class representing a team member."""
-        def __init__(self, lookup, first_name, last_name):
-            self.lookup = lookup
-            self.first_name = first_name
-            self.last_name = last_name
+class _Config(dict):
+    """Class representing a configuration."""
 
     @classmethod
     def from_yaml(cls, yaml_str):
-        """Return a team created from a yaml string."""
+        """Return a configuration created from a yaml string."""
         data = yaml.load(yaml_str)
         team = cls()
         for item in data:
@@ -24,22 +16,38 @@ class Team(dict):
 
     @classmethod
     def from_file(cls, fpath):
-        """Return a team read in from file."""
+        """Return a configuration read in from file."""
         return cls.from_yaml(file(fpath))
 
     @property
     def lookups(self):
-        """Return set of lookup aliases in the team."""
+        """Return set of lookup aliases in the configuration."""
         return set(self.keys())
 
-    def add_member(self, lookup, first_name, last_name):
-        """Add a team member."""
+    def add_member(self, *args, **kwargs):
+        """Add a configuration member."""
+        lookup = None
+        if "lookup" in kwargs:
+            lookup = kwargs["lookup"]
+        elif len(args) > 0:
+            lookup = args[0]
+        else:
+            raise(RuntimeError("No lookup provided"))
         if lookup in self:
             raise(KeyError("Lookup {} already in use".format(lookup)))
-        self[lookup] = self.Member(lookup, first_name, last_name)
+        self[lookup] = self.Member(*args, **kwargs)
 
     def member(self, lookup):
-        """Return the team member associated with the lookup."""
+        """Return the configuration member associated with the lookup."""
         return self[lookup]
 
 
+class Team(_Config):
+    """Class representing an agile team."""
+
+    class Member(object):
+        """Class representing a team member."""
+        def __init__(self, lookup, first_name, last_name):
+            self.lookup = lookup
+            self.first_name = first_name
+            self.last_name = last_name

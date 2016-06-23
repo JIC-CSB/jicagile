97bfbeb3e997fbd7989e2672c38946b62ba62192
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2016-06-23 22:59:30 +0100

    Updated version number to 0.4.0

--- jicagile/__init__.py
+++ jicagile/__init__.py
@@ -9,7 +9,7 @@ from slugify import slugify
 
 from config import Team, Themes
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 
 class Task(dict):
--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ class NoseTestCommand(TestCommand):
         import nose
         nose.run_exit(argv=['nosetests'])
 
-version = "0.3.0"
+version = "0.4.0"
 readme = open('README.rst').read()
 
 
